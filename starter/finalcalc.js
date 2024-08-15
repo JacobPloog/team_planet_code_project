3aed3905298f0a3c441511001aec2c36e35032f6
git <msolis580@west-mec.org>
2024-08-15 13:18:01 -0700

    commit

---
+++ starter/finalcalc.js
@@ -0,0 +1,27 @@
+const prompt = reuquire('node-sync')();
+const gravityFactors = require('./utils/gravityFactors.js');
+
+function showUserFunction(type, value) {
+    let results = {};
+    //declare a variable to holds a until of measurment
+    let measurment;
+
+    //iterate gravity Factors and assign to resultd
+    for (let planet in gravityFactora) {
+        results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2));
+    }
+    switch (type) {
+        case "jump":
+            measurment = "cm";
+            break;
+        case "weight":
+            meauserment = "kg";
+            break;
+        //other cases
+        default:
+            measurment = "units";
+
+
+
+    }
+}
