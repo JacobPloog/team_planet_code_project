d8890be56823769ed22104cc398044ef4a5b626b
Jacob Ploog <jploog066@west-mec.org>
2024-08-15 13:38:13 -0700

    commit

--- starter/jake_code.js
+++ starter/jake_code.js
@@ -62,7 +62,7 @@ function showUserFactors(type, value) {
         default:
             measurement = "units"
     }
-    console.log("")
+    console.log("your jump on planet is " + value(measurement))
 }
 
 global.showUserFactors = showUserFactors;
