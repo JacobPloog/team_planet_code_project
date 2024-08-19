4696005afe24a7d511524e0129b7636f709ecdcd
Jacob Ploog <jploog066@west-mec.org>
2024-08-19 12:57:02 -0700

    commit

--- starter/jake_code.js
+++ starter/jake_code.js
@@ -1,54 +1,71 @@
 const prompt = require('prompt-sync')();
-const gravityFactors = require('./utils/earthGravityFactors.js');
+const earthGravityFactors = require('./utils/earthGravityFactors.js');
 const alienGravityFactors = require('./utils/alienGravityFactors.js');
 const validWords = ["metric", "imperial", "bob", "kevin"];
 
-function showUserFactors(type, units, value) {
+function showUserFactors(factorType, factorUnit, factorValue) {
     let results = {};
     let measurement;
     let system;
-    for (let planet in gravityFactors) {
-        results[planet] = parseFloat((gravityFactors[planet] * value).toFixed(2));
+    for (let planet in earthGravityFactors) {
+        results[planet] = parseFloat((earthGravityFactors[planet] * factorValue).toFixed(2));
     }
     for (let planet in alienGravityFactors) {
-        results[planet] = parseFloat((alienGravityFactors[planet] * value).toFixed(2));
+        results[planet] = parseFloat((alienGravityFactors[planet] * factorValue).toFixed(2));
     }
-    switch (units) {
-        case "metric":
-            system = "metric"
+    switch (factorUnit) {
+        case "1":
+            system = "metric";
             break;
-        case "imperial":
-            system = "imperial"
+        case "2":
+            system = "imperial";
             break;
     }
-    switch (type) {
+    switch (factorType) {
         case "jump":
-            if (system = "metric") { measurement = "cm" } else { measurement = "in" }
+            for (i = 0; i < validWords.length; i++) {
+                if (system == "metric") {
+                    measurement = "cm";
+                    break;
+                } else if (measurement == "imperial") {
+                    measurement = "in";
+                    break;
+                }
+            }
             break;
         case "weight":
-            if (system = "metric") { measurement = "kg" } else { measurement = "lb" }
+            for (i = 0; i < validWords.length; i++) {
+                if (system == "metric") {
+                    measurement = "kg";
+                    break;
+                } else if (measurement == "imperial") {
+                    measurement = "lb";
+                    break;
+                }
+            }
             break;
         case "pushups":
-            measurement = " pushups"
+            measurement = " pushups";
             break;
         default:
-            measurement = " units"
+            measurement = " units";
             break;
     }
     for (let planet in results) {
-        console.log(`your ${type} on ${planet} is ${results[planet]}${measurement}`);
+        console.log(`your ${factorType} on ${planet} is ${results[planet]}${measurement}`);
     }
 }
 function getUserInput() {
-    console.log("Metric or Imperial measurement?");
-    let units = prompt(">> ").trim().toLowerCase();
+    console.log("Metric (1) or Imperial (2) measurement?");
+    let factorUnit = prompt(">> ").trim().toLowerCase();
     console.log("What type of measurement would you like to use? (weight, jump, or pushups)");
-    let type = prompt(">> ").trim().toLowerCase();
-    console.log(`What is your ${type} on earth?`);
-    let value = prompt(">> ").trim().toLowerCase();
-    console.log(showUserFactors(type, units, value));
+    let factorType = prompt(">> ").trim().toLowerCase();
+    console.log(`What is your ${factorType} on earth?`);
+    let factorValue = prompt(">> ").trim().toLowerCase();
+    console.log(showUserFactors(factorType, factorUnit, factorValue));
 }
-
 
 global.showUserFactors = showUserFactors;
 global.getUserInput = getUserInput;
+
+getUserInput();
