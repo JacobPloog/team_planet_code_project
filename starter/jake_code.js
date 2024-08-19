97437900e000191c89c99b9f49906bd1c2111f36
Jacob Ploog <jploog066@west-mec.org>
2024-08-19 14:27:15 -0700

    not done

--- starter/jake_code.js
+++ starter/jake_code.js
@@ -1,68 +1,81 @@
+// jake code
+
 const prompt = require('prompt-sync')();
 const earthGravityFactors = require('./utils/earthGravityFactors.js');
 const alienGravityFactors = require('./utils/alienGravityFactors.js');
-const validWords = ["metric", "imperial", "bob", "kevin"];
 
-function showUserFactors(factorType, factorUnit, factorValue) {
+function showUserFactors(factorType, factorPlanets, factorSystem, factorValue) {
     let results = {};
-    let measurement;
     let system;
+    let measurement;
     for (let planet in earthGravityFactors) {
         results[planet] = parseFloat((earthGravityFactors[planet] * factorValue).toFixed(2));
     }
     for (let planet in alienGravityFactors) {
         results[planet] = parseFloat((alienGravityFactors[planet] * factorValue).toFixed(2));
     }
-    switch (factorUnit) {
-        case "1":
-            system = "metric";
-            break;
-        case "2":
-            system = "imperial";
-            break;
-    }
-    switch (factorType) {
-        case "jump":
-            for (i = 0; i < validWords.length; i++) {
-                if (system == "metric") {
-                    measurement = "cm";
-                    break;
-                } else if (measurement == "imperial") {
-                    measurement = "in";
-                    break;
-                }
-            }
-            break;
-        case "weight":
-            for (i = 0; i < validWords.length; i++) {
-                if (system == "metric") {
-                    measurement = "kg";
-                    break;
-                } else if (measurement == "imperial") {
-                    measurement = "lb";
-                    break;
-                }
-            }
-            break;
-        case "pushups":
-            measurement = " pushups";
-            break;
-        default:
-            measurement = " units";
-            break;
-    }
     for (let planet in results) {
         console.log(`your ${factorType} on ${planet} is ${results[planet]}${measurement}`);
     }
 }
+
+
 function getUserInput() {
-    console.log("Metric (1) or Imperial (2) measurement?");
-    let factorUnit = prompt(">> ").trim().toLowerCase();
-    console.log("What type of measurement would you like to use? (weight, jump, or pushups)");
-    let factorType = prompt(">> ").trim().toLowerCase();
-    console.log(`What is your ${factorType} on earth?`);
-    let factorValue = prompt(">> ").trim().toLowerCase();
-    console.log(showUserFactors(factorType, factorUnit, factorValue));
+    let factorSystem = "repetitions";
+    let factorType;
+    let factorValue;
+    let factorPlanets;
+    while (true) {
+        if (factorType !== "pushups") {
+            console.log("Metric (1) or Imperial (2) measurement?");
+            factorSystem = prompt(">> ").trim().toLowerCase();
+        }
+        if (!isNaN(factorSystem) && 3 > factorSystem < 0) {
+            switch (factorSystem) {
+                case "1":
+                    factorSystem = "metric"
+                    break;
+                case "2":
+                    factorSystem = "imperial";
+                    break;
+                default:
+                    break;
+            }
+            break;
+        } else {
+            console.error("WRONG");
+        }
+    }
+    let factorUnit = "repetitions";
+    if (factorsystem === "metric") {
+        if (factorType === "weight") {
+            factorUnit = "kg"
+        } else if (factorType === "jump") {
+            factorUnit = "cm"
+        }
+    } else if (factorSystem === "imperial") {
+        if (factorType === "weight") {
+            factorUnit = "lbs";
+        } else if (factorType === "jump") {
+            factorUnit = "in";
+        }
+    }
+    while (true) {
+        console.log("What type of measurement would you like to use? (weight, jump, or pushups)");
+        factorType = prompt(">> ").trim().toLowerCase();
+        break;
+    }
+    while (true) {
+        console.log(`What is your ${factorType} on earth?`);
+        factorValue = prompt(">> ").trim().toLowerCase();
+        break;
+    }
+    while (true) {
+        console.log(`What solar system would you like to use? (earth or alien)`);
+        factorPlanets = prompt(">> ").trim().toLowerCase();
+        break;
+    }
+    console.log(showUserFactors(factorType, factorPlanets, factorSystem, factorValue));
 }
 
 global.showUserFactors = showUserFactors;
